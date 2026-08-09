ea6f250071da305b0a67fc18e1ad2fc0d8a784ee
karansolase <126546501+karansolase@users.noreply.github.com>
2026-08-09 14:26:14 +0530

    Restore models serializers admin setup

--- 2_Models_Serializers_Admin_Setup_CSS/backend/store/models.py
+++ 2_Models_Serializers_Admin_Setup_CSS/backend/store/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model) : 
   name = models.CharField(max_length=100, unique=True)
+  # slug looks like  apple-iphone-17-pro-max
   slug = models.SlugField(unique=True)
 
   def __str__(self) : 
