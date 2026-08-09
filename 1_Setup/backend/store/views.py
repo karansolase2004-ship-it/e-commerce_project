6fb9fc1921eeb22db6061c6cfd87df17e9a89f91
karansolase2004-ship-it <karansolase2004@gmail.com>
2026-08-09 13:40:03 +0530

    Delete 1_Setup directory

--- 1_Setup/backend/store/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse 
-
-# Create your views here.
-def home(request) : 
-  data = {
-    'message' : 'Welcome to the E-commerce Store!'
-  }
-  return JsonResponse(data)
