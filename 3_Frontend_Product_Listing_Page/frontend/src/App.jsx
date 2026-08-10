91e34bd866bf1d3d0d1a423645e227211d62c5e9
karansolase <126546501+karansolase@users.noreply.github.com>
2026-08-10 21:50:44 +0530

    Add 3_Frontend_Product_Listing_Page

---
+++ 3_Frontend_Product_Listing_Page/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+import Productlist from '../pages/Productlist';
+
+function App(){
+  return (
+    <div>
+      <Productlist/>
+    </div>
+  )
+}
+
+export default App
