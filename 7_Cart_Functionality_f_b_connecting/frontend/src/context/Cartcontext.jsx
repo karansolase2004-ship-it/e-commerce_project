32774a09252a58e8e5050fe4c0887c30bea15645
karansolase <126546501+karansolase@users.noreply.github.com>
2026-08-13 22:36:24 +0530

    Add Cart_Functionality_connectingfrontend_backend

---
+++ 7_Cart_Functionality_f_b_connecting/frontend/src/context/Cartcontext.jsx
@@ -0,0 +1,91 @@
+import { createContext, useContext, useState, useEffect } from "react";
+const CartContext = createContext();
+
+export const Cartprovider = ({ children }) => {
+  const BASEURL = import.meta.env.VITE_DJANGO_BASE_URL;
+  const [cartItems, setCartItems] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  // Fetch Cart from BE 
+  const fetchCart = async () => {
+    try{
+      const res = await fetch(`${BASEURL}/api/cart`)
+      if(!res.ok)
+        {
+          throw new Error("Failed to fetch cart");
+        }  
+        const data = await res.json();
+        setCartItems(data.items || [] );
+        setTotal(data.total || 0);
+    }
+    catch(error)
+    {
+      console.error("Error fetching Cart : ", error);
+    } 
+  }
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
+  const addtocart = async (productId) => {
+    try{
+      await fetch(`${BASEURL}/api/cart/add/`, {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json",
+        },
+        body : JSON.stringify({product_id : productId}),
+      });
+      fetchCart();
+    }catch (error){
+      console.error("Error Adding to cart : ", error);
+    }
+  };
+
+  const removeFromCart = async(itemId) => {
+    try{
+      await fetch(`${BASEURL}/api/cart/remove/`, {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json",
+        },
+        body : JSON.stringify({item_id : itemId}),
+      });
+      fetchCart();
+    }
+    catch(error){
+      console.error("Error Removing from cart: ", error);
+    }
+  }
+
+  const updateQuantity = async(itemId, quantity) => {
+    if(quantity < 1){
+      await removeFromCart(itemId);
+      return; 
+    }
+    try{
+      await fetch(`${BASEURL}/api/cart/update/`, {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json",
+        },
+        body : JSON.stringify({item_id : itemId, quantity}),
+      });
+      fetchCart();
+    }
+    catch (error)
+    {
+      console.error("Error updating quantity:", error);
+    }
+  };
+
+  return(
+    <CartContext.Provider
+      value={{cartItems, total, addtocart, removeFromCart, updateQuantity}}>
+        {children}
+    </CartContext.Provider>
+  )
+};
+
+export const useCart = () => useContext(CartContext);
